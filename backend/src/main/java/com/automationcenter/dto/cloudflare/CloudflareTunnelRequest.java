485dbf63318f9fa32109cff8c6a64d76e5bf1046
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-05-08 22:53:49 -0300

    fix: add all-args constructor to CloudflareTunnelRequest

--- backend/src/main/java/com/automationcenter/dto/cloudflare/CloudflareTunnelRequest.java
+++ backend/src/main/java/com/automationcenter/dto/cloudflare/CloudflareTunnelRequest.java
@@ -1,9 +1,13 @@
 package com.automationcenter.dto.cloudflare;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CloudflareTunnelRequest {
     @NotBlank
     private String name;
